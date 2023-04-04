3f2a6c3c0c1fa6e700135f246d23cf2278295530
User <filataanna@mail.ru>
2023-04-04 21:57:08 +0300

    one commit

---
+++ src/main/java/com/list/homework25/Employee.java
@@ -0,0 +1,40 @@
+package com.list.homework25;
+
+import java.util.List;
+
+public class Employee {
+
+        private final String firstName;
+        private final String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
